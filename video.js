0954c06c2941f7319d007a6e621c006dd519e853
Tyrone McNichols <tytymc3@gmail.com>
2020-10-17 16:39:41 -0700

    Canvas added

--- video.js
+++ video.js
@@ -1,8 +1,16 @@
 const url_input = document.getElementById("url_input")
 const url_input_button = document.getElementById("url_input_button")
-const video_footage = document.getElementById("footage")
+const video_footage = document.getElementById("footage-img")
+const footage_canvas = document.getElementById("footage");
+footage_canvas.width = 1000;
+footage_canvas.height = 1000;
 let footage_error = false;
 let url = url_input.value
+
+    
+let ctx = footage_canvas.getContext("2d");
+
+
 video_footage.onerror = () => {
     footage_error = true;
     alert("Something went wrong looking for your URL")
@@ -27,3 +35,11 @@ function change_footage() {
         video_footage.style.display = 'block'
     }
 }
+
+function update_canvas() {
+    ctx.clearRect(0, 0, footage_canvas.width, footage_canvas.height)
+    ctx.drawImage(video_footage, 0, 0, footage_canvas.width, footage_canvas.height)
+    setTimeout(update_canvas, 20)
+}
+
+update_canvas();
