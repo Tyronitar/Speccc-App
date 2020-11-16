d78c6475d02e421c2721709b8c43d3ddb3548a80
Tyrone McNichols <tytymc3@gmail.com>
2020-11-15 17:09:58 -0800

    Added icon and updated package.json

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ const path = require('path')
 let win
 
 function createWindow() {
-   win = new BrowserWindow({show: false})
+   win = new BrowserWindow({
+      show: false,
+      icon: 'assets/rainbow.ico'
+   })
    win.maximize()
    win.show()
    win.setMinimumSize(960, 600)
    win.loadURL(url.format ({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
-      slashes: true
+      slashes: true,
    }))
    win.webContents.openDevTools() // use this line to see dev console
 }
