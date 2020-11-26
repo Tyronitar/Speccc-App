bad468f001eb2dc4dc1a35450eb11effb7c2120a
Tyrone McNichols <tytymc3@gmail.com>
2020-11-25 22:19:46 -0800

    Doing some funky stuff with electron-forge

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
 const {app, BrowserWindow} = require('electron')
 const url = require('url')
 const path = require('path')
+const os = require('os');
+if(require('electron-squirrel-startup')) return;
+
+const platforms = {
+  WINDOWS: 'WINDOWS',
+  MAC: 'MAC',
+  LINUX: 'LINUX',
+  SUN: 'SUN',
+  OPENBSD: 'OPENBSD',
+  ANDROID: 'ANDROID',
+  AIX: 'AIX',
+};
+
+const platformsNames = {
+  win32: platforms.WINDOWS,
+  darwin: platforms.MAC,
+  linux: platforms.LINUX,
+  sunos: platforms.SUN,
+  openbsd: platforms.OPENBSD,
+  android: platforms.ANDROID,
+  aix: platforms.AIX,
+};
+
+const currentPlatform = platformsNames[os.platform()];
 
 let win
 
 function createWindow() {
-   win = new BrowserWindow({
+   options = {
       show: false,
-      icon: 'assets/rainbow.ico'
-   })
+      icon: 'assets/icon.ico'
+   }
+   if (currentPlatform === 'MAC') {
+      options.icon = "assets/icon.icns"
+   }
+   else if (currentPlatform === 'LINUX') {
+      options.icon = "assets/icon.png"
+   }
+   win = new BrowserWindow(options)
    win.maximize()
    win.show()
    win.setMinimumSize(960, 600)
@@ -21,3 +52,9 @@ function createWindow() {
 }
 
 app.on('ready', createWindow)
+
+// makes app stay open with unopen windows on macOS
+app.on(
+   "window-all-closed",
+   () => process.platform !== "darwin" && app.quit() // "darwin" targets macOS only.
+);
